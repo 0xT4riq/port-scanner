964b90e783eaea34b796441c6e6b6f701df0b2e7
Tariq alharrasi <58836862+Tariqalharrasi@users.noreply.github.com>
2025-05-01 22:22:35 +0400

    improve the efficiency of threading

--- scanner.py
+++ scanner.py
@@ -3,34 +3,66 @@ import sys
 import socket
 import threading
 import datetime 
-
+import ipaddress
+from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor
 
 ascii_banner = pyfiglet.figlet_format("Port Scanner",font="epic")
 print(ascii_banner)
 print("_" * 60)
-info_banner = pyfiglet.figlet_format("by 0xT4riq",font="straight")
+info_banner = pyfiglet.figlet_format("by 0xT4riq",font="term")
 info_banner += "\nGitHub: https://github.com/0xT4riq\nVersion: 1.0\n\nUse responsibly. Unauthorized scanning is illegal.\n"
 print(info_banner)
 print("_" * 60)
 
 
-targer = input(str("Enter the target IP: "))
-print("-" * 60)
-print("Scanning target: " + targer)
-print("Scanning started at: " + str(datetime.datetime.now()))
 
-try:
-    for port in range(1, 65535):
+target = input(str("Enter the target IP: "))
+open_ports = []
+def scan_port(port):
+    try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         socket.setdefaulttimeout(0.5)
 
-        result = sock.connect_ex((targer, port))
+        result = sock.connect_ex((target, port))
         if result == 0:
-            print(f"Port {port} is open".format(port))
+            open_ports.append(port)
         sock.close()
-except KeyboardInterrupt:
-    print("\nExiting program.")
-    sys.exit()
-except socket.error:
-    print("Could not connect to server. Please check the target IP.")
-    sys.exit()
+    except KeyboardInterrupt:
+        print("\nExiting program.")
+        sys.exit()
+    except socket.error:
+        print("Could not connect to server. Please check the target IP.")
+        sys.exit()
+
+
+
+
+
+def main():
+
+    try:
+        ipaddress.ip_address(target)
+    except ValueError:
+        print("Invalid IP address format. Please enter a valid IP address.")
+        sys.exit()
+
+    print("-" * 60)
+    print("Scanning target: " + target)
+    print("Scanning started at: " + str(datetime.datetime.now()))
+
+
+    start_port = int(input("Enter the starting port (or -1 for all): "))
+    if start_port < 0:
+        start_port = 1
+        end_port = 65535
+    else:
+        end_port = int(input("Enter the ending port: "))
+    ports = list(range(start_port, end_port ))
+    with ThreadPoolExecutor(max_workers=100) as executor:
+        list(tqdm(executor.map(scan_port, ports), total=len(ports)))
+    print("\nOpen ports:")
+    for port in sorted(open_ports):
+        print(port)
+
+main()
